17e26bde9adc5276adcf015e7b94821efdc3d057
Denis Kivilev <kivilev.d@gmail.com>
2024-09-14 12:55:21 +0300

    Fix CORS issue

---
+++ practice_tasks/kiwi_wallet_applications/open-api-kivi-wallet/src/main/kotlin/ru/oralcedbd/openapikiviwallet/config/WebConfig.kt
@@ -0,0 +1,16 @@
+package ru.oralcedbd.openapikiviwallet.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+    }
+}
